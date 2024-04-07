2d2995cfddba86ddd72b71a60293841a48d67340
ETadl007 <ETadl007@gmail.com>
2024-04-07 17:35:43 +0800

    update vite.config.js -> base = graduation-project

--- myBlog/vite.config.js
+++ myBlog/vite.config.js
@@ -7,7 +7,7 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers"; // 按�
 
 
 export default defineConfig({
-  base: "./",
+  base: "graduation-project",
   root: process.cwd(), // 绝对路径
   resolve: {
     // 配置路径别名
