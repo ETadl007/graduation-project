9319bdcb0ecc26140c3d1b06e46a89836fea446a
ETadl007 <131686538+ETadl007@users.noreply.github.com>
2024-04-18 18:02:48 +0800

    first commit

--- server/src/main.js
+++ server/src/main.js
@@ -4,7 +4,7 @@ const app = express();
 
 //开放跨域请求
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Credentials", "true");
