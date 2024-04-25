e23fa45c2ec90db1b1d3729520890958b38594c1
ETadl007 <131686538+ETadl007@users.noreply.github.com>
2024-04-25 23:11:26 +0800

    delete footer ICP filing information

--- blog-v3/src/components/Layout/footer/blog-footer.vue
+++ blog-v3/src/components/Layout/footer/blog-footer.vue
@@ -2,36 +2,10 @@
   <!-- 最外层 div 是占位置的 -->
   <div class="footer_box flex flex-col items-center justify-center">
     <!-- eslint-disable-next-line -->
-    <div class="footer-color">&copy 小张的博客 2023</div>
+    <div class="footer-color">&copy 小炮 2024-4-1</div>
     <a class="footer-color change-color mt-[5px]" href="http://beian.miit.gov.cn/" target="_blank"
-      >蜀ICP备2023007772号</a
+      >2814753920</a
     >
-    <div class="footer-color mt-[5px] flex justify-center flex-wrap">
-      <a
-        class="p-[3px]"
-        href="https://www.aliyun.com/?spm=5176.28055625.J_3207526240.1.6a27154aw7v5VK"
-        target="_blank"
-      >
-        <img
-          src="https://img.shields.io/badge/%E9%98%BF%E9%87%8C%E4%BA%91-ECS%E6%9C%8D%E5%8A%A1%E5%99%A8-orange"
-          alt=""
-      /></a>
-      <a class="p-[3px]" href="https://imzbf.github.io/md-editor-v3/docs/index" target="_blank">
-        <img
-          src="https://img.shields.io/badge/MdEditorV3-MD%E7%BC%96%E8%BE%91%E5%99%A8-159957"
-          alt=""
-      /></a>
-      <a class="p-[3px]" href="https://butterfly.js.org/" target="_blank">
-        <img src="https://img.shields.io/badge/HEXO-BUTTERFLY-blue" alt="" />
-      </a>
-      <a class="p-[3px]" href="https://min.io/" target="_blank">
-        <img
-          src="https://img.shields.io/badge/minio-%E5%AF%B9%E8%B1%A1%E5%AD%98%E5%82%A8%E7%B3%BB%E7%BB%9F-purple
-"
-          alt=""
-        />
-      </a>
-    </div>
   </div>
 </template>
 
